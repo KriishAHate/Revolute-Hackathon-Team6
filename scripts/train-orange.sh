52efe2cc033bf873db44d27ab44bb27f02e45611
Philip Knodle <pknodle@gmail.com>
2026-08-02 01:25:05 -0400

    Fixed script to use the correct dataset.
    The script used the run of 4, not 50.

--- scripts/train-orange.sh
+++ scripts/train-orange.sh
@@ -1,2 +1,2 @@
 
-lerobot-train   --dataset.repo_id=orange-fixed_4   --policy.type=act   --output_dir=outputs/train/act_orange-50   --job_name=act_your_dataset   --policy.device=cuda   --wandb.enable=false   --policy.push_to_hub=false --steps=50000
+lerobot-train   --dataset.repo_id=orange-fixed-50   --policy.type=act   --output_dir=outputs/train/act_orange-50-run2   --job_name=act_your_dataset   --policy.device=cuda   --wandb.enable=false   --policy.push_to_hub=false --steps=50000

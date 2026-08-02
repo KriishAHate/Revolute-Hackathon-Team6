ff97c35112994721d17eac576e572467485c458e
Philip Knodle <pknodle@gmail.com>
2026-08-02 01:31:29 -0400

    Added lychee Scripts

---
+++ scripts/train-lychee.sh
@@ -0,0 +1,2 @@
+
+lerobot-train   --dataset.repo_id=lychee-fixed-10   --policy.type=act   --output_dir=outputs/train/act_lychee   --job_name=act_your_dataset   --policy.device=cuda   --wandb.enable=false   --policy.push_to_hub=false --steps=50000

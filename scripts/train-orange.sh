19982d049da4776ba00db9cb47ce24c5a7f6b66a
Philip Knodle <pknodle@gmail.com>
2026-08-02 01:21:43 -0400

    Added training scripts

---
+++ scripts/train-orange.sh
@@ -0,0 +1,2 @@
+
+lerobot-train   --dataset.repo_id=orange-fixed_4   --policy.type=act   --output_dir=outputs/train/act_orange-50   --job_name=act_your_dataset   --policy.device=cuda   --wandb.enable=false   --policy.push_to_hub=false --steps=50000
